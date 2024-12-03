7cd71d8b0e70dc457fdfb9e971b078ce092e058f
Ayan Williams <101142116+Ionwill@users.noreply.github.com>
2024-12-03 14:58:59 -0600

    Starting Story challenge
    Starting story challenge. Separating logic amongst the MVC, creating struct

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,25 +14,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let stories = [
-            
-            Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
-            Story(title: "See a tiger", choice1: "Shout for help", choice2: "Play dead"),
-            Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
-
-            ]
-        
-        storyLabel.text = stories[0].storyTitle
-        choice1Button.setTitle(stories[0].choice1Text, for: .normal)
-        choice2Button.setTitle(stories[0].choice2Text, for: .normal)
-
+        storyLabel.text = storyBrain.stories[0].storyTitle
+        choice1Button.setTitle(storyBrain.stories[0].choice1Text, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[0].choice2Text, for: .normal)
     }
 
+    
     @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle
+        let choice1 = storyBrain.stories[0].choice1Text
+        let choice2 = storyBrain.stories[0].choice2Text
+        
+        storyBrain.getStoryNumber()
+        
+        if userChoice == choice1{
+            storyLabel.text = storyBrain.stories[1].storyTitle
+            choice1Button.setTitle(storyBrain.stories[1].choice1Text, for: .normal)
+            choice2Button.setTitle(storyBrain.stories[1].choice2Text, for: .normal)
+        }
+        if userChoice == choice2{
+            storyLabel.text = storyBrain.stories[2].storyTitle
+            choice1Button.setTitle(storyBrain.stories[2].choice1Text, for: .normal)
+            choice2Button.setTitle(storyBrain.stories[2].choice2Text, for: .normal)
+            
+        }
+        
+       
     }
     
 }
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,8 +8,29 @@
 
 import Foundation
 
-//let example = [
-//    
-////    Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right")
-//
-//    ]
+
+struct StoryBrain{
+    
+    let stories = [
+        Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+        Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+        Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
+        ]
+    
+    
+    mutating func getStoryNumber(){
+        let a = stories[0]
+        print(a)
+        
+        let storyNumber = stories[0].storyTitle.startIndex
+        print(storyNumber)
+        
+    }
+    
+
+}
+
+
+
+
+
